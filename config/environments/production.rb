6a8a27e0ad8a78dd41319e97e4dd5924d0f6263b
dmitryS1666 <dsuschinsky@gmail.com>
2026-02-19 11:33:00 +0300

    fix paths in puma.service and enable file logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
   # Отключено для работы с kamal-proxy (SSL будет на уровне proxy)
   config.force_ssl = false
 
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  # Log to STDOUT and file
+  config.logger = ActiveSupport::Logger.new(Rails.root.join("log", "production.log"))
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
